97c0d9a90464cafdf9fbedc28d2da7763960e81f
UltraWhats <contato@ultrawhats.com.br>
2025-06-06 14:53:17 -0300

    Node 20 Padrao
    Node 20 Padrao

--- instalador_single.sh
+++ instalador_single.sh
@@ -723,7 +723,7 @@ instala_node_base() {
   {
     sudo su - root <<EOF
   curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-  sudo sh -c "echo deb https://deb.nodesource.com/node_16.x focal main \
+  sudo sh -c "echo deb https://deb.nodesource.com/node_20.x focal main \
   > /etc/apt/sources.list.d/nodesource.list"
   sudo apt-get update && apt-get install nodejs -y
 EOF
